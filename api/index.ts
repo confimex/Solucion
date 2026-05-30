11546c5ab027d3e1c6be991083dfc3a9aa501ff3
confimex <adolfoms yahoo.com>
2026-05-30 16:13:15 -0600

    Fix worker-entry filename

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,3 @@
-import handler from "../dist/server/assets/worker-entry-BfGI7yAS.js";
+import handler from "../dist/server/assets/worker-entry-DfGDEsSQ.js";
 export const config = { runtime: "edge" };
 export default async function (req: Request) { return handler.fetch(req); }
