92580cebf6f7ea6be9d5cf74c2500939885fe92d
confimex <adolfoms yahoo.com>
2026-06-02 00:15:47 -0600

    Debug exports

--- api/index.ts
+++ api/index.ts
@@ -4,6 +4,6 @@ import * as workerEntry from "../dist/server/assets/worker-entry-DfGDEsSQ.js";
 export const config = { runtime: "nodejs" };
 
 export default async function (req: Request) {
-  const handler = workerEntry.default ?? workerEntry;
-  return handler(req);
+  const keys = Object.keys(workerEntry);
+  return new Response(JSON.stringify(keys), { status: 200 });
 }
