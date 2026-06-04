e8d5e641da42e0eb0181b1f636d02788d2f0c6e2
confimex <adolfoms yahoo.com>
2026-06-03 19:29:45 -0600

    Versión cloude

--- api/index.ts
+++
@@ -1,9 +0,0 @@
-// @ts-ignore
-import * as workerEntry from "../dist/server/assets/worker-entry-DfGDEsSQ.js";
-
-export const config = { runtime: "nodejs" };
-
-export default async function (req: Request) {
-  const keys = Object.keys(workerEntry);
-  return new Response(JSON.stringify(keys), { status: 200 });
-}
